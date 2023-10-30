dea57d4813ebd5dd5cfe7e0bb46fa2e9b1556b72
darksky <skynoelwee@gmail.com>
2023-10-30 11:18:31 +0800

    fix model training (add gpu support)

--- python/model_training.py
+++ python/model_training.py
@@ -1,7 +1,7 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = ''
+# os.environ['CUDA_VISIBLE_DEVICES'] = ''
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 
-import signal
 import tensorflow as tf
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.models import Model
@@ -10,16 +10,9 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.metrics import CategoricalAccuracy
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.callbacks import EarlyStopping, LambdaCallback, ModelCheckpoint, LearningRateScheduler
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-
-# Function to export the current model to TensorFlow Lite
-def export_to_tflite(model, tflite_filename):
-    converter = tf.lite.TFLiteConverter.from_keras_model(model)
-    tflite_model = converter.convert()
-    with open(tflite_filename, 'wb') as f:
-        f.write(tflite_model)
-    print("Model exported to", tflite_filename)
+import os
 
 # Define the path to your dataset (each subfolder is a food category)
 data_dir = './dataset'
@@ -28,7 +21,7 @@ data_dir = './dataset'
 batch_size = 32
 data_generator = ImageDataGenerator(
     preprocessing_function=preprocess_input,
-    validation_split=0.2  # You can adjust the validation split
+    validation_split=0.2
 )
 
 train_data = data_generator.flow_from_directory(
@@ -57,22 +50,44 @@ x = Dense(train_data.num_classes, activation='softmax')(x)
 # Create the model
 model = Model(inputs=base_model.input, outputs=x)
 
-# Compile the model
-model.compile(optimizer=Adam(learning_rate=0.001),
+# Check if there are any existing checkpoints
+if os.path.exists('best_model.h5'):
+    # Load the best checkpointed model
+    model.load_weights('best_model.h5')
+    print("Resuming training from the best checkpointed model.")
+    resume_training = True
+else:
+    # Start a new training session
+    resume_training = False
+
+# Compile the model with a lower learning rate
+initial_learning_rate = 0.001
+lr_schedule = LearningRateScheduler(lambda epoch: initial_learning_rate / (10 ** (epoch // 10)))
+model.compile(optimizer=Adam(learning_rate=initial_learning_rate),
               loss=CategoricalCrossentropy(from_logits=True),
               metrics=[CategoricalAccuracy()])
 
-# Define a ModelCheckpoint callback to save the best model
-model_checkpoint = ModelCheckpoint("best_model.h5", monitor='val_categorical_accuracy', save_best_only=True)
+# Define a LambdaCallback to print accuracy during training
+print_accuracy = LambdaCallback(on_epoch_end=lambda epoch, logs: print(f"Epoch {epoch + 1}, Val Acc: {logs['val_categorical_accuracy']}"))
 
-# Interrupt signal handler to export the model on keyboard interrupt
-def handle_interrupt(signum, frame):
-    export_to_tflite(model, 'interrupted_model.tflite')
-    exit(0)
+# Define model checkpoint to save the best model weights
+model_checkpoint = ModelCheckpoint('best_model.h5', save_best_only=True, monitor='val_categorical_accuracy', mode='max', verbose=1)
 
-# Set up the interrupt signal handler
-signal.signal(signal.SIGINT, handle_interrupt)
-
-# Train the model
+# Train the model with learning rate scheduling and checkpointing
 early_stopping = EarlyStopping(monitor='val_categorical_accuracy', patience=10, restore_best_weights=True)
-model.fit(train_data, validation_data=validation_data, epochs=100, callbacks=[early_stopping, model_checkpoint])
+
+try:
+    if resume_training:
+        # If resuming training, the initial_epoch is set to the last trained epoch
+        model.fit(train_data, validation_data=validation_data, epochs=100, callbacks=[early_stopping, print_accuracy, lr_schedule, model_checkpoint], initial_epoch=model.optimizer.iterations.numpy() // (train_data.samples // batch_size))
+    else:
+        model.fit(train_data, validation_data=validation_data, epochs=100, callbacks=[early_stopping, print_accuracy, lr_schedule, model_checkpoint])
+except KeyboardInterrupt:
+    print("Keyboard interrupt detected. Saving checkpoint and exporting to TFLite.")
+    model.save_weights('interrupted_model.h5')
+
+# Export the model to TensorFlow Lite format
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+with open('food_classifier.tflite', 'wb') as f:
+    f.write(tflite_model)

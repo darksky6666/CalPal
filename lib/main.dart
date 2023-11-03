f3ab94af35b83f6ae67cb25c7dcbd259e2161bb4
darksky <skynoelwee@gmail.com>
2023-11-04 01:51:25 +0800

    add navigator history (pop old routes when route > 5)

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:calpal/screens/home/home_view.dart';
 import 'package:calpal/controllers/login_state.dart';
 import 'package:calpal/screens/profile/profile_view.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/scheduler.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
@@ -29,6 +30,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      navigatorObservers: [CustomNavigatorObserver()],
       // Define your routes
       routes: {
         '/': (context) => LoginState(),
@@ -41,5 +43,23 @@ class MyApp extends StatelessWidget {
       },
       initialRoute: '/', // Set the initial route
     );
+  }
+}
+
+class CustomNavigatorObserver extends NavigatorObserver {
+  final List<Route<dynamic>> _routes = [];
+
+  @override
+  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    // Track the pushed routes
+    _routes.add(route);
+
+    // Check if the count exceeds 5 and remove the oldest route
+    if (_routes.length > 5) {
+      SchedulerBinding.instance.addPostFrameCallback((_) {
+        Navigator.of(route.navigator!.context).removeRoute(_routes.first);
+        _routes.removeAt(0);
+      });
+    }
   }
 }
